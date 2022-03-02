232751139a90512f5b79db15c8f73715b1945ad6
reismgadsden <rgadsden287@gmail.com>
2022-03-02 14:15:01 -0500

    First commit

---
+++ nussinov-base-pairs.py
@@ -0,0 +1,24 @@
+"""
+Nussinov base pairs.
+Program that predicts a rna sequence's secondary structure.
+
+:author - Reis Gadsden
+:version - 02/03/2022
+:github - https://github.com/reismgadsden/nussinov-base-pairs
+
+:class - CS-5531 @ Appalachian State University
+:instructor - Mohammad Mohebbi
+"""
+
+
+# needed imports
+import numpy as np
+
+
+# main method that will 'probably' make calls to all other methods
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
