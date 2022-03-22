080afbd00632c1dbe9d61de705815cc193109b9c
reismgadsden <rgadsden287@gmail.com>
2022-03-22 00:19:57 -0400

    FINALLY implemented backtracking and made sure that the score table was getting filled correctly with regards to the loop length

--- nussinov-base-pairs.py
+++ nussinov-base-pairs.py
@@ -3,7 +3,7 @@ Nussinov base pairs.
 Program that predicts a rna sequence's secondary structure.
 
 :author - Reis Gadsden
-:version - 02/03/2022
+:version - 21/03/2022
 :github - https://github.com/reismgadsden/nussinov-base-pairs
 
 :class - CS-5531 @ Appalachian State University
@@ -17,7 +17,7 @@ import numpy as np
 
 # main method that will 'probably' make calls to all other methods
 def main() -> None:
-    s = "CAUG"
+    s = "UUCUCCAGUGAGACGAAAACCAAUGCCUUCACUGGCAGUAACACCCAGCUUCCUGAACGCAUGUCAUGCAUGCCAGGUUUUGUUGGUU"
     max_base_pairs(s)
 
 
@@ -25,55 +25,89 @@ def max_base_pairs(s) -> None:
     N = np.empty((len(s), len(s)))
     N.fill(np.NaN)
 
+    BT = np.empty((len(s), len(s)))
+    BT.fill(np.NaN)
+
     for i in range(0, len(s)):
         N[i, i] = 0
+        BT[i, i] = -2
         if i != 0:
             N[i, i-1] = 0
-    print(np.matrix(get_secondary_struct(N, s)))
+            BT[i, i - 1] = -2
+    fill_score_array(N, BT, s, 5)
+    print(calc_second_struct(BT, 0, len(s) - 1, len(s) * "."))
 
 
-def get_secondary_struct(N, s) -> None:
+def fill_score_array(N, BT, s, min_loop_len) -> None:
     for j in range(1, len(s)):
         for i in range(0, len(s) - j):
-            #print("i = " + str(i) + "; j =" + str(j + i) + ";")
-            N[i, j + i] = get_max(N, s, i, j + i)
+            N[i, j + i] = get_max(N, BT, s, i, j + i, min_loop_len)
     return N
 
 
-def get_max(N, s, i, j):
-    #print("i = " + str(i) + "; j =" + str(j) + ";")
+def get_max(N, BT, s, i, j, min_loop_len):
+    if i  >= j - min_loop_len:
+        BT[i][j] = -2
+        return 0
     first_case = N[i + 1, j]
 
     k_arr = []
     for k in range(i + 1, j+1):
-        if i == 2 and j == 4:
-            print("i: " + str(i) + "; j:" + str(j) + "; k: " + str(k))
-            print("k -> " + str(k) + "letters: " + s[i] + ", " + s[k])
         if is_pair(s[i], s[k]):
-            print("k = " + str(k) + ";")
             k_arr.append(k)
 
     x = 0
     if k_arr:
         for k in k_arr:
             temp = 0
-            if (k + 1) < len(s):
-                temp = N[i + 1, k - 1] + N[k + 1, j]
+            if i >= k - min_loop_len:
+                x = -1
+                BT[i][j] = -2
             else:
-                temp = N[i + 1, k - 1]
-            x = max(temp, x)
+                if (k + 1) < len(s):
+                    temp = N[i + 1, k - 1] + N[k + 1, j]
+                else:
+                    temp = N[i + 1, k - 1]
+                if temp > x or temp == x:
+                    BT[i][j] = k
+                x = max(temp, x)
         x += 1
 
+    if first_case > x or first_case == x:
+        BT[i][j] = -1
     return max(first_case, x)
+
+
+def calc_second_struct(BT, i, j, output) -> str:
+
+    if i >= len(output) or j >= len(output):
+        return output
+    else:
+        k = BT[i][j]
+        if k == -2:
+            return output
+        elif k == -1:
+            output = calc_second_struct(BT, i+1, j, output)
+            return output
+        elif k == np.NaN:
+            print("Something has gone terribly wrong")
+            return output
+        else:
+            k = int(k)
+            output = output[0:i] + "(" + output[i+1:len(output)]
+            output = output[0:k] + ")" + output[k+1:len(output)]
+            output = calc_second_struct(BT, i + 1, k - 1, output)
+            output = calc_second_struct(BT, k + 1, j, output)
+            return output
 
 
 def is_pair(c1, c2) -> bool:
     look_up = {
-        'A': ['U','T'],
+        'A': ['U'],
         'C': ['G'],
-        'G': ['C', 'U'],
+        'G': ['C'],
         'T': ['A'],
-        'U': ['A', 'G']
+        'U': ['A']
     }
 
     return c2 in look_up[c1]
